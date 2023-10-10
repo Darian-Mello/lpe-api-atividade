620acd2358feebd4e13cef7ecfae167003a2448a
darian <darian.mello25@gmail.com>
2023-10-10 09:31:47 -0300

    Adicionado os scripts do banco

--- script_banco.sql
+++ script_banco.sql
@@ -11,31 +11,6 @@ create table acao (
    foreign key (atividade_id) references atividade (codigo)
 );
 
--- inserindo registros
--- categorias 
-insert into categorias (nome) values ('Eletrônicos') , ('Eletrodomésticos') , ('Informática');
-
--- produtos
-
-insert into produtos (nome, descricao, quantidade_estoque, ativo, valor, data_cadastro, categoria)
-values ('Mouse USB','Mouse USB', 20, true, 60.0, current_date,1), 
-('Mouse Sem FIO','Mouse sem fio', 10, true, 120.0, current_date,1),
-('Teclado USB','Teclado USB', 30, true, 500.0, current_date,1);
-
--- avaliações
-insert into avaliacoes (autor, email, texto, nota, data, produto) 
-values  ('Jorge', 'jorgebavaresco@ifsul.edu.br','Mouse muito preciso', 5, current_date, 1);
-
-
--- consultas
-
-select p.codigo as codigo, p.nome as nome, p.descricao as descricao, p.quantidade_estoque as quantidade_estoque, p.ativo as ativo, p.valor as valor, to_char(p.data_cadastro,'YYYY-MM-DD') as data_cadastro, p.categoria as categoria, c.nome as categoria_nome
-from produtos p
-join categorias c on p.categoria = c.codigo
-order by p.codigo;
-
-select * from avaliacoes;
-
 -- criação da tabela usuários
 create table usuario (
 	email varchar(50) not null primary key, 
